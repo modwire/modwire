1856c869edb6a00eaeb8ae4cb8070bbeaaadb032
Tomasz Szpak <tomszp@gmail.com>
2026-07-09 06:19:04 +0200

    add collectors

--- src/modwire/architecture/app.py
+++ src/modwire/architecture/app.py
@@ -5,7 +5,7 @@ from wireup import Inject, injectable
 from modwire.shared import config
 
 from .boundaries import FlowReportCollector
-from .map import ArchitectureMapLoader
+from .map import ArchitectureMapLoader, MapReportCollector
 
 
 @injectable(lifetime="transient")
@@ -14,8 +14,10 @@ class ArchitectureApplication:
         self,
         config: Annotated[config.ArchitectureConfig, Inject(config="architecture")],
         map_loader: ArchitectureMapLoader,
+        map_report_collector: MapReportCollector,
         flow_report_collector: FlowReportCollector,
     ):
         self.config = config
         self.map_loader = map_loader
+        self.map_report_collector = map_report_collector
         self.flow_report_collector = flow_report_collector
